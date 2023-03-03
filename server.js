71ab8369c6322cb60b0147b6e6d3f13f514ccbff
Globik <globalikslivov@gmail.com>
2023-03-03 22:39:02 +0500

    new

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const axios=require('axios').default;
+const app = express()
+app.use(express.static('./'));
+const port = 3000
+
+app.get('/wp-content/themes/greenEx/php/calc.php', (req, res) => {
+	console.log("QUERY: ", req.query);
+	axios.get(`https://greenex.pro/wp-content/themes/greenEx/php/calc.php`, {params: req.query}).then(
+	function(d){
+		console.log("data: ",d.data)
+		res.json(d.data)
+		}).catch(function(er){
+			console.log('err: ',er.toString())
+			res.send(er.toString());
+			})
+
+  
+})
+
+let a = new Map();
+a.set(1, {price: 1});
+a.set(2, {price: 2.20});
+a.set(3, {price: 3});
+console.log("size: ", a.size);
+let c = [];
+a.forEach(function(el,i){
+	c.push(el.price);
+})
+console.log("array: ", c)
+let b = c.reduce(function(ac, v){
+	
+	var returns = ac + v;
+	return returns;
+}, 10)
+console.log("total sum: ", b.toFixed(2))
+let da = "504.10";
+console.log("da: ", Number(parseFloat(da).toFixed(2)))
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
