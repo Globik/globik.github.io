c5c34dbcfe271844fc08c397beb9c7fa5158ae34
Globik <globalikslivov@gmail.com>
2026-08-23 08:39:46 +0500

    a

--- static/js/my.js
+++ static/js/my.js
@@ -1,1 +1,8 @@
 console.log(1)
+
+window.yaContextCb.push(() => {
+    Ya.Context.AdvManager.render({
+        "blockId": "R-A-14728017-1",
+        "renderTo": "yandex_rtb_R-A-14728017-1"
+    })
+})
