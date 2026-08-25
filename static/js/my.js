b995d3481c9440ac22f5f52fee7ea507b29c2872
Globik <globalikslivov@gmail.com>
2026-08-25 13:37:59 +0500

    a

--- static/js/my.js
+++ static/js/my.js
@@ -1,1 +1,37 @@
 console.log(1)
+
+
+window.yaContextCb.push(() => {
+    Ya.Context.AdvManager.render({
+        "blockId": "R-A-14728017-2",
+        "type": "fullscreen",
+        "platform": "touch"
+    })
+})
+
+window.yaContextCb.push(() => {
+    Ya.Context.AdvManager.render({
+        "blockId": "R-A-14728017-3",
+        "type": "fullscreen",
+        "platform": "desktop"
+    })
+})
+<!-- Yandex.RTB R-A-14728017-4 -->
+
+window.yaContextCb.push(() => {
+    Ya.Context.AdvManager.render({
+        "blockId": "R-A-14728017-4",
+        "type": "floorAd",
+        "platform": "touch"
+    })
+})
+
+window.yaContextCb.push(() => {
+    Ya.Context.AdvManager.render({
+        "blockId": "R-A-14728017-5",
+        "type": "floorAd",
+        "platform": "desktop"
+    })
+})
+
+
